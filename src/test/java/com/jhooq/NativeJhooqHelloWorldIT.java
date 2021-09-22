621eead10ca807ba27242a43f63cb1438d669cb5
prathibhapadma <prathibha.padma@capgemini.com>
2021-09-22 09:09:30 +0000

    commited

---
+++ src/test/java/com/jhooq/NativeJhooqHelloWorldIT.java
@@ -0,0 +1,9 @@
+package com.jhooq;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeJhooqHelloWorldIT extends JhooqHelloWorldTest {
+
+    // Execute the same tests but in native mode.
+}
